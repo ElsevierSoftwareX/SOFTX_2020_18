1859d5824f79da8519246ec982521c7ae34deb4b
ahmedsleem8000 <44513663+ahmedsleem8000@users.noreply.github.com>
2020-01-22 15:22:28 +0200

    Update urls.py

--- src/nsets/ivns/urls.py
+++ src/nsets/ivns/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('norm', views.norm, name='norm'),
-    path('ahp', views.ahp, name='ahp'),
-    path('similarity', views.similarity, name='similarity'),
 ]
